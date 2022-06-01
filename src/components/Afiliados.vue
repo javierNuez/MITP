9fe3eb1aede8b6565d586ccd38c921747976d50a
Javi <javier.nuez91@gmail.com>
2022-05-31 21:14:21 -0300

    se creo el obj en afiliados

--- src/components/Afiliados.vue
+++ src/components/Afiliados.vue
@@ -1,16 +1,25 @@
 <template>
     <div>
         <h3>Afiliados</h3>
-        
+        {{lista}}
+        <button @click="traerDatos">Traer datos</button>
     </div>
 </template>
 <script>
+import conectarAfiliados, {} from '../services/conectarAfiliados.js'
 export default {
     data() {
         return {
+            lista:[],
             afiliado: { id_paciente: 0, nombre: '', apellido: '', dni: 0 }
         }
         
+    },
+    methods:{
+        async traerDatos(){
+            const rta= await (await conectarAfiliados.getAfiliados()).data;
+            console.log(rta);
+        }
     }
 }
 </script>
