3e006b15bca8c4a0e846de5d6e56d5b068bb3af3
JavierBeta <javier.nuez91@gmail.com>
2022-06-03 13:43:01 -0300

    se agrego tabla en turnos

--- src/components/Turnos.vue
+++ src/components/Turnos.vue
@@ -1,13 +1,35 @@
 <template>
-    <div>
+    <div style="width:70%;margin:0 auto;">
         <h3>Turnos</h3>
-        <li v-for="conectarTurnos in listaT" :key="conectarTurnos.id_afiliado">
-         
-        {{conectarTurnos.id_afiliado}} {{conectarTurnos.nombreA}} 
-        {{conectarTurnos.apellidoA}} {{conectarTurnos.dniA}}
-		{{conectarTurnos.nombreM}} {{conectarTurnos.apellidoM}}
-		{{conectarTurnos.especialidadM}}
-		</li>
+		<table class="table"  border="1">
+		<thead>
+		<tr  class="table-primary">
+			<th scope="col" style="text-align: center;">Afiliado</th>
+			<th scope="col" style="text-align: center;">Nombre</th>
+			<th scope="col" style="text-align: center;">Apellido</th>
+			<th scope="col" style="text-align: center;">DNI</th>
+			<th scope="col" style="text-align: center;">Médico Nombre</th>
+			<th scope="col" style="text-align: center;">Médico Apellido</th>
+			<th scope="col" style="text-align: center;">Especialidad</th>
+			<th scope="col" style="text-align: center;">Fecha Turno</th>
+			<th scope="col" style="text-align: center;">Hora Turno</th>
+		</tr>
+		</thead>
+		
+		<tbody>
+		<tr v-for="conectarTurnos in listaT" :key="conectarTurnos.id_afiliado">
+		<td>{{conectarTurnos.id_afiliado}}</td>
+		<td>{{conectarTurnos.nombreA}}</td>
+		<td>{{conectarTurnos.apellidoA}}</td>
+		<td>{{conectarTurnos.dniA}}</td>
+		<td>{{conectarTurnos.nombreM}}</td>
+		<td>{{conectarTurnos.apellidoM}}</td>
+		<td>{{conectarTurnos.especialidadM}}</td>
+		<td>{{conectarTurnos.fecha}}</td>
+		<td>{{conectarTurnos.hora}}</td>
+		</tr>
+		</tbody>
+		</table>         
         {{mensajeError}}
         <button @click="traerDatos">Traer datos</button>
     </div>
@@ -18,7 +40,7 @@ export default {
     data() {
         return {
             listaT:[],
-            Turno: { id_afiliado: 0, nombreA: '', apellidoA: '', dniA: 0 ,nombreM:'', apellidoM:'', especialidadM:''},
+            Turno: { id_afiliado: 0, nombreA: '', apellidoA: '', dniA: 0 ,nombreM:'', apellidoM:'', especialidadM:'',fecha:'',hora:''},
             mensajeError: ''
         }
         
